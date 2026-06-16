18c01cc356167737f70527b2d14125d6ae3d212b
unknown <abhaykumar2007singh@gmail.com>
2026-06-16 15:39:58 +0530

    change in cors

--- app/main.py
+++ app/main.py
@@ -234,7 +234,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
-        "https://owallet.vercel.app",
+        "https://o-wallet-three.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
